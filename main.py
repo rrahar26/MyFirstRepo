04577a86f39c65b98b04d3ced981985ae1888d46
Rahul <rahul.rahar@yahoo.com>
2026-08-25 17:19:54 +0530

    adding my change in the feature branch

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from utils import calculator
 if __name__ == "__main__":
     print(calculator.add_numbs(2, 3))
+    print(calculator.subtract_numbs(5, 2))
+    print(calculator.multiply_numbs(4, 3))
+    print(calculator.divide_numbs(10, 2))
