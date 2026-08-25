69abb6a0475b82a985260fbd1f8c961e6510c881
Rahul <rahul.rahar@yahoo.com>
2026-08-25 17:28:08 +0530

    adding the labels

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from utils import calculator
 if __name__ == "__main__":
     print(calculator.add_numbs(2, 3))
+    """returns the sum of two numbers"""
     print(calculator.subtract_numbs(5, 2))
+    """returns the difference of two numbers"""
     print(calculator.multiply_numbs(4, 3))
+    ""returns the product of two numbers"""
     print(calculator.divide_numbs(10, 2))
+    """returns the quotient of two numbers"""
